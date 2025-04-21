f1d78f225cf9dd9899b88c83d9446114c0b7b58f
johnwesly08 <johnweslypd@gmail.com>
2025-04-21 15:44:06 +0530

    Authentication UI changes made complaints Backend is added

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,4 +1,6 @@
 class AppRoutes {
   static const String auth = '/auth';
   static const String userhome = '/userhome';
+  static const String adminLogin = '/admin-login';
+  static const String guestLogin = '/admin-guest-login';
 }
